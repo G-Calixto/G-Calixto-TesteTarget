8b48a4dbc6b2c96dbffeafdf1f9668f1fe05c085
G-Calixto <gntdc@ic.ufal.br>
2024-09-04 04:07:08 -0300

    Adicionado READMME

---
+++ 1 - Soma de K a Indice/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int indice = 13;
+    int soma = 0;
+    int k = 0;
+    for(k; k<indice;k++){
+        soma += k;
+    }
+    printf("%d\n", soma);
+
+
+
+    return 0;
+}
+
+
